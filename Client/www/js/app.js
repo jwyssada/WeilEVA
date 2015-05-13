74f2c6fd5d170037b80d81ac5ea1604c47916079
salztorte <olli.franzen@gmx.de>
2015-05-13 08:00:45 +0200

    ...

--- Client/www/js/app.js
+++ Client/www/js/app.js
@@ -1,13 +1,14 @@
+
 'use strict';
 var app = angular.module('appName', ['ui.router'])
-	.run()
-	.config(function($stateProvider, $urlRouterProvider){
-				$urlRouterProvider.otherwise('/index');
+    .run()
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider.otherwise('/index');
 
-				$stateProvider
-					.state('index', {
-						url         : '/index',
-						templateUrl : './www/templates/hallo.html'
-					});
+        $stateProvider
+            .state('index', {
+                url: '/index',
+                templateUrl: './www/templates/hallo.html'
+            });
 
-			});
+    });
